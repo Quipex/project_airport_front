b5a3ddbe20bdd9037ddea6726ba008183002955b
json96ua <42866143+json96ua@users.noreply.github.com>
2018-12-11 14:01:56 +0200

    Add airline's model

---
+++ src/app/shared/models/airlines.model.ts
@@ -0,0 +1,9 @@
+export class AirlinesModel {
+  constructor(
+    public name?: string,
+    public descr?: string,
+    public email?: string,
+    public phonenumber?: string,
+    public id?: number
+  ) {}
+}
