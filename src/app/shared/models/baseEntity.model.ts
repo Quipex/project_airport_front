b7cbb70c8a8a72289d8a2a7611381e9fe2067437
json96ua <42866143+json96ua@users.noreply.github.com>
2018-12-18 15:27:10 +0200

    Add extra fields into BaseEntity

--- src/app/shared/models/baseEntity.model.ts
+++ src/app/shared/models/baseEntity.model.ts
@@ -1,6 +1,10 @@
 export class BaseEntityModel {
   constructor(
-    public id?: number
+    public id?: number,
+    public objectId?: number,
+    public parentId?: number,
+    public objectName?: string,
+    public objectDescription?: string,
   ) {}
 
 }
