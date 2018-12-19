35e831fd1aad3b535965e31100f0de2a582ddd80
json96ua <42866143+json96ua@users.noreply.github.com>
2018-12-19 20:39:03 +0200

    Add model and service for airplane's component

---
+++ src/app/shared/models/airplanes.model.ts
@@ -0,0 +1,7 @@
+export class AirplanesModel {
+  constructor(
+    public id?: number,
+    public model?: string,
+    public airlineId?: number
+  ) {}
+}
---
+++ src/app/shared/services/airplanes.service.ts
@@ -0,0 +1,43 @@
+import {Injectable} from "@angular/core";
+import {BaseService} from "./baseService.service";
+import {BaseEntityModel} from "../models/baseEntity.model";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {AuthenticationService} from "./authentication.service";
+
+@Injectable()
+export class AirplanesService implements BaseService {
+
+  private httpOptions = {
+    headers: new HttpHeaders({
+      'Authorization': 'Bearer ' + this.authenticationService.getToken(),
+      'Content-Type': 'application/json'
+    })
+  };
+
+  constructor(
+    private http: HttpClient,
+    private authenticationService: AuthenticationService
+  ) {}
+
+
+  addItem(baseEntity: BaseEntityModel) {
+    return this.http.post('http://localhost:8080/airplanes', baseEntity, this.httpOptions);
+  }
+
+  deleteItem(id: number) {
+    return this.http.delete(`http://localhost:8080/airplanes/${id}`, this.httpOptions);
+  }
+
+  editItem(id: number, baseEntity: BaseEntityModel) {
+    return this.http.put(`http://localhost:8080/airplanes`, baseEntity, this.httpOptions);
+  }
+
+  getCountOfItems() {
+    return this.http.get('http://localhost:8080/airplanes/count', this.httpOptions);
+  }
+
+  getTenItems(page: number) {
+    return this.http.get(`http://localhost:8080/airplanes/page=${page}`, this.httpOptions);
+  }
+
+}
