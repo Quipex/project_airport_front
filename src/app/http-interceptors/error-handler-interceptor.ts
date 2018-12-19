c2d34ca66ff8c8e0ce2c3462ba902f7453de263f
do-While-on-GitHub <atterdag@zoho.eu>
2018-12-19 09:41:06 +0200

    Now error-handler catch and display in toastr all http 4xx, 5xx errors

--- src/app/http-interceptors/error-handler-interceptor.ts
+++ src/app/http-interceptors/error-handler-interceptor.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpRequest, HttpInterceptor, HttpHandler, HttpEvent, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { HttpRequest, HttpInterceptor, HttpHandler, HttpEvent, HttpErrorResponse } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
-import { catchError } from 'rxjs/operators';
+import {catchError, retry} from 'rxjs/operators';
 import { ToastrService } from 'ngx-toastr';
 
 @Injectable()
@@ -9,27 +9,44 @@ export class ErrorHandlerInterceptor implements HttpInterceptor {
 
   constructor(private toastr: ToastrService) { }
 
-  handleError(error: HttpErrorResponse) {
-    this.toastr.error('Unknown error  ', error.status.toString());
-    return throwError(error);
+  showWarningToastr(error: HttpErrorResponse) {
+    this.toastr.warning(error.error.message, error.status.toString());
+  }
+
+  showErrorToastr(error: HttpErrorResponse) {
+    this.toastr.error(error.error.message, error.status.toString());
+  }
+
+  showErrorToastrWithCustomMessage(message: string) {
+    this.toastr.error(message);
   }
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-
     return next.handle(request).pipe(
+      // retry will repeat request if catch error response
+      retry(1),
       catchError( (error: HttpErrorResponse) => {
         let errMsg = '';
         // Client Side Error
         if (error.error instanceof ErrorEvent) {
-          errMsg = `Error: ${error.error.message}`;
-          this.toastr.error('Something went wrong. Check your internet connection  ', error.status.toString());
+          errMsg = `Error: ${error.message}`;
+          console.log(error.message);
+          this.showErrorToastrWithCustomMessage('Client side error');
         } else {
           // Server Side Error
-          if (error.status === 0) {
-            errMsg = `Error Code: ${error.status},  Message: ${error.message}`;
-            this.toastr.error('Unknown error  ', error.status.toString());
-          } else if (error.status === 401) {
-            this.toastr.error('Usfdffsdr  ', error.status.toString());
+          if (!navigator.onLine) {
+            this.showErrorToastrWithCustomMessage('Check your internet connection');
+          } else {
+            if (error.status === 0) {
+              errMsg = `Error Code: ${error.status},  Message: ${error.message}`;
+              this.toastr.error('Unknown error');
+            } else if ((error.status / 100) >= 4 && (error.status / 100) < 5) {
+              errMsg = `Error: ${error.message}`;
+              this.showWarningToastr(error);
+            } else if ((error.status / 100) >= 5) {
+              errMsg = `Error: ${error.message}`;
+              this.showErrorToastr(error);
+            }
           }
         }
         return throwError(errMsg);
