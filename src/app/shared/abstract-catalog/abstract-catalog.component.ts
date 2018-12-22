105f9f0b95deef9190a274357be20a2314dcd674
Quipex <rudas72@gmail.com>
2018-12-23 00:50:53 +0200

    fixed id variable name

--- src/app/shared/abstract-catalog/abstract-catalog.component.ts
+++ src/app/shared/abstract-catalog/abstract-catalog.component.ts
@@ -1,16 +1,16 @@
 import {Component, Input, OnInit, ViewChild} from '@angular/core';
 import {BaseService} from '../services/baseService.service';
-import {Router} from '@angular/router';
 import {UsersService} from '../services/users.service';
 import {AirlinesService} from '../services/airlines.service';
 import {ToastrService} from 'ngx-toastr';
-import {FormBuilder, FormGroup} from '@angular/forms';
+import {FormGroup} from '@angular/forms';
 import {BaseEntityModel} from '../models/baseEntity.model';
 import {ColumnSetting} from '../models/columnSetting.model';
 import {ModalDirective} from 'angular-bootstrap-md';
 import {InputBaseModel} from '../models/inputBase.model';
 import {ResponseErrorModel} from '../models/responseError.model';
 import {FormControlService} from "../services/formControl.service";
+
 @Component({
   selector: 'app-project-center',
   templateUrl: './abstract-catalog.component.html',
@@ -42,7 +42,8 @@ export class AbstractCatalogComponent implements OnInit {
     private airlinesService: AirlinesService,
     private toastr: ToastrService,
     private fcs: FormControlService
-  ) {}
+  ) {
+  }
 
   ngOnInit() {
     this.service.getCountOfItems()
@@ -55,7 +56,6 @@ export class AbstractCatalogComponent implements OnInit {
       });
     this.form = this.fcs.toFormGroup(this.questions);
   }
-
 
 
   private getTenItems(numberOfPage: number, direction: boolean) {
@@ -105,8 +105,10 @@ export class AbstractCatalogComponent implements OnInit {
       .subscribe(() => {
         const message = 'The item has been deleted.';
         this.showInfo(message);
+      }, err => {
+        this.responseError = err;
+        this.showError(this.responseError.error.message);
       });
-
   }
 
   onSave(returnedItem: BaseEntityModel) {
@@ -125,15 +127,17 @@ export class AbstractCatalogComponent implements OnInit {
             this.responseError = err;
             this.showError(this.responseError.error.message);
           });
-
     } else {
       this.newModal.hide();
-      this.service.editItem(returnedItem.id, returnedItem)
+      this.service.editItem(returnedItem.objectId, returnedItem)
         .subscribe((editedItem: BaseEntityModel) => {
           this.entities[this.selectedRow] = editedItem;
           this.entities = JSON.parse(JSON.stringify(this.entities));
           const message = 'The item has been edited.';
           this.showInfo(message);
+        }, err => {
+          this.responseError = err;
+          this.showError(this.responseError.error.message);
         });
     }
     this.showNew = false;
