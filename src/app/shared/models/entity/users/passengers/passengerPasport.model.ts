6de05c1a1d7bbc522d3e8513f6e73c462f2d41cc
json96ua <42866143+json96ua@users.noreply.github.com>
2019-01-10 18:24:02 +0200

    Add passenger and passport models

---
+++ src/app/shared/models/entity/users/passengers/passengerPasport.model.ts
@@ -0,0 +1,10 @@
+import {BaseEntityModel} from "../../../baseEntity.model";
+import {PassengersModel} from "./passengers.model";
+import {PassportModel} from "./passport.model";
+
+export class PassengerPassportModel extends BaseEntityModel{
+  constructor(
+    public passenger?: PassengersModel,
+    public passport?: PassportModel
+  ) { super();}
+}
---
+++ src/app/shared/models/entity/users/passengers/passengerPassportCommon.model.ts
@@ -0,0 +1,13 @@
+import {BaseEntityModel} from "../../../baseEntity.model";
+
+export class PassengerPassportCommonModel extends BaseEntityModel{
+  constructor(
+    public firstName?: string,
+    public lastName?: string,
+    public serialNumber?: string,
+    public country?: string,
+    public birthDate?: Date,
+  ) {
+    super();
+  }
+}
