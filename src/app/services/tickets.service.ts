6bbdb67ccbed8c7b5f7f01a08a8119128631b5de
json96ua <42866143+json96ua@users.noreply.github.com>
2019-01-22 15:56:24 +0200

    Add ticket's service

---
+++ src/app/services/tickets.service.ts
@@ -0,0 +1,29 @@
+import {Injectable} from "@angular/core";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {AuthenticationService} from "./authentication.service";
+import {FilterAndSortWrapperModel} from "../shared/models/filterAndSortWrapper.model";
+import {TicketDTOModel} from "../shared/models/ticketDTO.model";
+
+@Injectable()
+export class TicketsService {
+  private httpOptions = {
+    headers: new HttpHeaders({
+      'Authorization': 'Bearer ' + this.authenticationService.getToken(),
+      'Content-Type': 'application/json'
+    })
+  };
+
+  constructor(
+    private http: HttpClient,
+    private authenticationService: AuthenticationService
+  ) {
+  }
+
+  search(page: number, wrapper: FilterAndSortWrapperModel) {
+    return this.http.post(`http://localhost:8080/tickets/search/page=${page}`, wrapper, this.httpOptions);
+  }
+
+  editTicket(item: TicketDTOModel) {
+    return this.http.put(`http://localhost:8080/tickets`, item, this.httpOptions);
+  }
+}
