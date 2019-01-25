9dd42def36c7973dc0f62d69991f25a09f87fcb2
json96ua <42866143+json96ua@users.noreply.github.com>
2019-01-25 12:25:03 +0200

    Other changes

--- src/app/airplanes/plane-seats-editor/plane-seats-editor.component.ts
+++ src/app/airplanes/plane-seats-editor/plane-seats-editor.component.ts
@@ -3,26 +3,34 @@ import {SEATS} from '../data/mock-seats';
 import {SeatModel} from '../../shared/models/entity/airplane/seat.model';
 import {of} from 'rxjs/internal/observable/of';
 import {AirplanesModel} from '../../shared/models/entity/airplane/airplanes.model';
-import {PLANES} from '../data/mock-airplanes';
 import {SeatTypeModel} from '../../shared/models/entity/airplane/seat-type.model';
 import {SEAT_TYPES} from '../data/mock-seat-types';
 import {SectionModel} from '../seats/plane-seats-grid/seat-type-section/section-model';
 import {ViewMode} from '../seats/plane-seats-grid/plane-seats-grid-modes.model';
+import {AirplanesService} from "../../services/airplanes.service";
+import {AirlinesService} from "../../services/airlines.service";
+import {ActivatedRoute} from "@angular/router";
+import {AirlinesModel} from "../../shared/models/entity/airline/airlines.model";
 
 @Component({
   selector: 'app-plane-seats-editor',
   templateUrl: './plane-seats-editor.component.html',
-  styleUrls: ['./plane-seats-editor.component.scss']
+  styleUrls: ['./plane-seats-editor.component.scss'],
+  providers: [AirplanesService, AirlinesService]
 })
 export class PlaneSeatsEditorComponent implements OnInit {
   public seats: Set<SeatModel>;
-  public plane: AirplanesModel;
+  public plane: AirplanesModel = new AirplanesModel();
   public seatTypes: SeatTypeModel[];
   public selectedSeats = new Set<SeatModel>();
   public sections: SectionModel[];
   viewMode = ViewMode.EDIT;
 
-  constructor() {
+  constructor(
+    private airplanesService: AirplanesService,
+    private airlinesService: AirlinesService,
+    private route: ActivatedRoute
+  ) {
   }
 
   ngOnInit() {
@@ -30,12 +38,24 @@ export class PlaneSeatsEditorComponent implements OnInit {
       this.seats = seats;
     });
 
-    of(PLANES).subscribe(plane => {
-      this.plane = plane[0];
-    });
-
     of(SEAT_TYPES).subscribe(seatTypes => {
       this.seatTypes = seatTypes;
+    });
+
+    this.getAirplane();
+  }
+
+  getAirplane() {
+    let sub = this.route.params.subscribe(params => {
+      let airplaneId = params['airplaneId'];
+      this.airplanesService.getItemById(airplaneId)
+        .subscribe((response: any) => {
+          this.plane.model = response.model;
+          this.airlinesService.getItemById(response.airlineId)
+            .subscribe((response: AirlinesModel) => {
+              this.plane.airline = response
+            });
+        });
     });
   }
 
