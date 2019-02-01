310f56f7531bc08bc1c46330f7b7966cdd44c644
do-While-on-GitHub <atterdag@zoho.eu>
2019-02-01 15:43:33 +0200

    Search methods for flight booking

--- src/app/booking/flight-booking/flight-booking.component.ts
+++ src/app/booking/flight-booking/flight-booking.component.ts
@@ -1,43 +1,72 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl} from '@angular/forms';
+import {environment} from '../../../environments/environment';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {FlightSearchWrapperModel} from '../../shared/models/flightSearchWrapper.model';
+import {FlightDTOModel} from '../../shared/models/flightDTO.model';
+import {AuthenticationService} from '../../services/authentication.service';
 
-/*import { FlightBookingService } from '../../services/flight-booking.service';
-import {SearchFlightModel} from '../../shared/models/searchFlight.model';
-import {CityModel} from '../../shared/models/entity/flight/city.model';*/
+const API_URL = environment.apiUrl;
 
 @Component({
   selector: 'app-flight-booking',
   templateUrl: './flight-booking.component.html',
   styleUrls: ['./flight-booking.component.scss'],
-  // providers: [FlightBookingService]
 })
 export class FlightBookingComponent implements OnInit {
 
   departureCity = '';
   destinationCity = '';
-  departureDate;
+  departureDate = new Date();
+  returnDate;
+  finalSearchString = '';
 
   currentDay = new Date().getDate();
   currentMonth = new Date().getMonth();
   currentYear = new Date().getFullYear();
-  date = new FormControl(new Date());
   minDate = new Date(this.currentYear, this.currentMonth, this.currentDay);
   flightTypes = [
     {'name': 'One way', id: 'oneway'},
     {'name': 'Round trip', id: 'round_trip'}
   ];
-  defaultFlightType = this.flightTypes[1].name;
+  defaultFlightType = this.flightTypes[0].name;
 
-  adultNum = 'option1';
-  childrenNum = 'option0';
-  ticketClass = 'option1';
+  private httpOptions = {
+    headers: new HttpHeaders({
+      'Authorization': 'Bearer ' + this.authenticationService.getToken(),
+      'Content-Type': 'application/json'
+    })
+  };
 
   constructor(
+    private http: HttpClient,
+    private authenticationService: AuthenticationService
   ) { }
 
   ngOnInit() {
   }
 
   searchFlight() {
+    if (this.defaultFlightType === 'One way') {
+      let wrapper = new FlightSearchWrapperModel(this.departureCity, this.destinationCity, this.departureDate);
+      console.log(this.departureCity, this.destinationCity, this.departureDate, this.returnDate)
+      this.searchOneWay(1, wrapper)
+        .subscribe((data: FlightDTOModel) => {
+          // TODO
+        });
+    } else if (this.defaultFlightType === 'Round trip') {
+      let wrapper = new FlightSearchWrapperModel(this.departureCity, this.destinationCity, this.departureDate, this.returnDate);
+      console.log(this.departureCity, this.destinationCity, this.departureDate, this.returnDate)
+      this.searchBoth(1, wrapper)
+        .subscribe((data: FlightDTOModel) => {
+          // TODO
+        });
+    }
+  }
+
+  searchOneWay(page: number, wrapper: FlightSearchWrapperModel) {
+    return this.http.post(API_URL + `/flight-booking/search-one-way/page=${page}`, wrapper, this.httpOptions);
+  }
+  searchBoth(page: number, wrapper: FlightSearchWrapperModel) {
+    return this.http.post(API_URL + `/flight-booking/search-both/page=${page}`, wrapper, this.httpOptions);
   }
 }
