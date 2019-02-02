315ad6558a1260ea86e94ea3085b312c335ba710
Quipex <rudas72@gmail.com>
2019-02-02 22:39:14 +0200

    deleted as not needed anymore

--- src/app/airplanes/data/mock-airlines.ts
+++
@@ -1,5 +0,0 @@
-import {AirlinesModel} from '../../shared/models/entity/airline/airlines.model';
-
-export const AIRLINES = [
-  new AirlinesModel('Airline1', 'descr1', 'email1', 'phNum1')
-];
--- src/app/airplanes/data/mock-airplanes.ts
+++
@@ -1,9 +0,0 @@
-import {AirplanesModel} from '../../shared/models/entity/airplane/airplanes.model';
-import {AIRLINES} from './mock-airlines';
-
-export const PLANES = [
-  new AirplanesModel('model-1', AIRLINES[0]),
-  new AirplanesModel('model-2', AIRLINES[0]),
-  new AirplanesModel('model-3', AIRLINES[0]),
-  new AirplanesModel('model-4', AIRLINES[0])
-];
