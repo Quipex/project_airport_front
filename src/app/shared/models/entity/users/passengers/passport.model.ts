3da53b08e516b1932b96af8a22c084ee9f46250d
Quipex <rudas72@gmail.com>
2019-02-07 10:18:11 +0200

    impl cloneable

--- src/app/shared/models/entity/users/passengers/passport.model.ts
+++ src/app/shared/models/entity/users/passengers/passport.model.ts
@@ -1,11 +1,18 @@
 import {BaseEntityModel} from '../../../baseEntity.model';
 
-export class PassportModel extends BaseEntityModel {
+export class PassportModel extends BaseEntityModel implements Cloneable {
   constructor(
     public serialNumber?: string,
     public country?: string,
     public birthDate?: string,
   ) {
     super();
+  }
+
+  clone(object: any) {
+    super.clone(object);
+    this.serialNumber = object.serialNumber;
+    this.country = object.country;
+    this.birthDate = object.birthDate
   }
 }
